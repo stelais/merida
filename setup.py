791a63a0992feeb984968780c25d09d42784d00d
Stela IS <stela.ishitani@gmail.com>
2024-03-22 12:25:22 -0400

    adding requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
 
 setup(name='merida',
-      version='0.1',
+      version='0.1.0',
       description='MERIDA: MOA9yr Exploration and Research Interface for Dataset Analysis Resources',
       url='https://github.com/stelais/merida',
       author='Stela IS',
+      author_email='stela.ishitanisilva@nasa.gov',
       license='MIT',
       packages=['merida'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=["pyarrow==14.0.1",
+                        "bokeh==3.1.0",
+                        "numpy==1.26.2",
+                        "pandas==2.1.4",
+                        "tqdm==4.66.1",
+                        "requests==2.31.0",
+                        "lxml==4.9.3",
+                        "html5lib==1.1"])
