02d5855d36caacfd8f6aedfe8636d2de9572f6f7
stelais <stela.ishitani@gmail.com>
2025-04-07 17:41:25 -0400

    little formatting / clean up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='merida',
-      version='0.2.1.5',
+      version='0.2.1.6',
       description='MERIDA: MOA9yr Exploration and Research Interface for Dataset Analysis Resources',
       url='https://github.com/stelais/merida',
       author='Stela IS, Greg O, and MERIDA authors',
