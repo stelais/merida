442f397578a6afe2da679a2ebcd01776c38af694
golmschenk <greg@olmschenk.com>
2025-04-27 00:08:16 -0400

    Make the "all light curve download" script parallelized by default. Update the readme to no longer use the removed ROW_NUM.

--- all_lightcurves_downloader.py
+++ all_lightcurves_downloader.py
@@ -1,10 +1,22 @@
+from multiprocessing import Pool
+
 from lightcurve_downloader import download_lightcurve
 from merida.lightcurves_cls import Metadata
 from tqdm import tqdm
 
-metadata = Metadata()
-df = metadata.dataframe
 
-for lightcurve_name in tqdm(df['lightcurve_name']):
-    print('\n', lightcurve_name, '\n')
-    download_lightcurve(lightcurve_name, '', path_to_save_='data/microlensing_2M/', lightcurve_extension_='.feather')
+def process_light_curve_name(light_curve_name):
+    print('\n', light_curve_name, '\n')
+    download_lightcurve(light_curve_name, '', path_to_save_='data/microlensing_2M/', lightcurve_extension_='.feather')
+
+
+def download_all_light_curves():
+    metadata = Metadata()
+    df = metadata.dataframe
+    with Pool(15) as pool:
+        for _ in tqdm(pool.imap_unordered(process_light_curve_name, df['lightcurve_name'])):
+            pass
+
+
+if __name__ == '__main__':
+    download_all_light_curves()
